7c850fa0dc70875cf97658b20412700c0a414631
spkaluzny <spkaluzny@gmail.com>
2023-01-08 12:10:03 -0800

    Added script to raw data files

--- data/zzz.R
+++ data/zzz.R
@@ -2,3 +2,7 @@ yard_birds <- read.table("merlin_yard_birds.csv2", sep=";", header=TRUE)
 yard_birds[["DateTime"]] <- as.POSIXct(yard_birds[["DateTime"]])
 pagliacci_delivery <- read.csv("pagliacci_delivery.csv")
 pagliacci_delivery[["Date"]] <- as.Date(pagliacci_delivery[["Date"]])
+worldle <- read.csv("worldle.csv")
+worldle[["Date"]] <- as.Date(worldle[["Date"]])
+worldle[["Country"]][(worldle[["Country"]] == "")] <- NA
+
